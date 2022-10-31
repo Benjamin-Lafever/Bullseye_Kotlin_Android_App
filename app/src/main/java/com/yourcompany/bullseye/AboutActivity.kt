aad322cfe96db42f6893944c0fd3a28ffb624ce5
Benjamin.Lafever <benlafever582@gmail.com>
2022-10-31 13:54:01 -0400

    Adding bullseye app

---
+++ app/src/main/java/com/yourcompany/bullseye/AboutActivity.kt
@@ -0,0 +1,23 @@
+package com.yourcompany.bullseye
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yourcompany.bullseye.databinding.ActivityAboutBinding
+import com.yourcompany.bullseye.databinding.ActivityMainBinding
+
+class AboutActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAboutBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAboutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        title = getString(R.string.about_page_title)
+
+        binding.goBackButton?.setOnClickListener{
+            finish()
+        }
+    }
+}
